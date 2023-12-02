5f850b8d7c2681537d73705620c47c9964e774dd
Dmitry286 <DShishlov@gmail.com>
2023-12-02 13:15:01 +0300

    labor-refactoring - исправлены @Qwery в методах репозиториев (добавлена аннотация @Param)

--- src/main/java/org/geoproject/ingeo/repositories/labor/ShearRepository.java
+++ src/main/java/org/geoproject/ingeo/repositories/labor/ShearRepository.java
@@ -5,6 +5,7 @@ import org.geoproject.ingeo.models.Sample;
 import org.geoproject.ingeo.models.labor.Shear;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,19 +17,20 @@ public interface ShearRepository extends JpaRepository<Shear, Long> {
     @Query("SELECT s FROM Shear s " +
             "WHERE s.sample.surveyPoint.project.id = :projectId " +
             "AND s.isArchive = false")
-    List<Shear> findAllByProjectIdAndIsNotArchive(Long projectId);
+    List<Shear> findAllByProjectIdAndIsNotArchive(@Param("projectId") Long projectId);
 
     @Query("SELECT s FROM Shear s " +
             "WHERE s.sample = :sample " +
             "AND s.shearPointNumber = :number " +
             "AND s.isArchive = false")
-    Optional<Shear> findBySampleAndShearPointNumber(Sample sample, Integer number);
+    Optional<Shear> findBySampleAndShearPointNumber(@Param("sample") Sample sample,
+                                                    @Param("number") Integer number);
 
     @Query("SELECT s FROM Shear s " +
             "WHERE s.sample.surveyPoint.project = :currentProject " +
             "AND s.isArchive = false " +
             "ORDER BY s.shearPointNumber ASC")
-    List<Shear> findByProjectSortByShearPointNumber(Project currentProject);
+    List<Shear> findByProjectSortByShearPointNumber(@Param("currentProject") Project currentProject);
 
     List<Shear> findBySampleOrderByShearPointNumber(Sample sample);
 }
--- src/main/java/org/geoproject/ingeo/repositories/labor/WaterExtractFullRepository.java
+++ src/main/java/org/geoproject/ingeo/repositories/labor/WaterExtractFullRepository.java
@@ -4,6 +4,7 @@ import org.geoproject.ingeo.models.Sample;
 import org.geoproject.ingeo.models.labor.WaterExtractFull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,16 +16,17 @@ public interface WaterExtractFullRepository extends JpaRepository<WaterExtractFu
     @Query("SELECT w FROM WaterExtractFull w " +
             "WHERE w.sample.surveyPoint.project.id = :projectId " +
             "AND w.isArchive = false")
-    List<WaterExtractFull> findAllByProjectIdAndIsNotArchive(Long projectId);
+    List<WaterExtractFull> findAllByProjectIdAndIsNotArchive(@Param(value = "projectId") Long projectId);
 
     @Query("SELECT w FROM WaterExtractFull w " +
             "WHERE w.sample.surveyPoint.pointNumber like :surveyPointNumber " +
             "AND w.sample.laborNumber like :laborNumber " +
             "AND w.isArchive = false")
-    Optional<WaterExtractFull> findBySurveyPointNumberAndLaborNumberAndIsNotArchive(String surveyPointNumber, String laborNumber);
+    Optional<WaterExtractFull> findBySurveyPointNumberAndLaborNumberAndIsNotArchive(@Param(value = "surveyPointNumber") String surveyPointNumber,
+                                                                                    @Param(value = "laborNumber") String laborNumber);
 
     @Query("SELECT w FROM WaterExtractFull w " +
             "WHERE w.sample = :sample " +
             "AND w.isArchive = false")
-    Optional<WaterExtractFull> findBySampleAndNotArchive(Sample sample);
+    Optional<WaterExtractFull> findBySampleAndNotArchive(@Param(value = "sample") Sample sample);
 }
